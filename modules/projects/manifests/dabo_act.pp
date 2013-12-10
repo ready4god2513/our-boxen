a1bd197b14cdf2f6b3dd290f4600d1b45720c8f7
Sean Knox <knoxville@gmail.com>
2013-12-09 16:50:47 -0800

    dabo_act project now prepares database.yml and .env, installs phantomjs

--- modules/projects/manifests/dabo_act.pp
+++ modules/projects/manifests/dabo_act.pp
@@ -1,10 +1,22 @@
 class projects::dabo_act {
-  include phantomjs
+  include phantomjs::1_9_0
 
   boxen::project { 'dabo_act':
     redis         => true,
     postgresql    => true,
     ruby          => '1.9.3-p484',
+    phantomjs     => '1.9.0',
     source        => 'dabohealth/dabo_act'
   }
+
+  file { "${boxen::config::srcdir}/dabo_act/config/database.yml":
+      content => template("/opt/boxen/repo/modules/projects/templates/dabo_act/database.yml.erb"),
+      require => Repository["${boxen::config::srcdir}/dabo_act"]
+  }
+
+  file { "${boxen::config::srcdir}/dabo_act/.env":
+      content => template("/opt/boxen/repo/modules/projects/templates/dabo_act/env.erb"),
+      require => Repository["${boxen::config::srcdir}/dabo_act"]
+  }
+
 }
