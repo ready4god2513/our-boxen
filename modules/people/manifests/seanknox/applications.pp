4f30b3e3d81f51598f30bf950594de7f8841acd6
Sean Knox <knoxville@gmail.com>
2014-02-19 10:28:59 -0800

    Clean up based on Puppet style guide

--- modules/people/manifests/seanknox/applications.pp
+++ modules/people/manifests/seanknox/applications.pp
@@ -76,38 +76,38 @@ class people::seanknox::applications {
   mysql::db { 'mydb': }
 
   ## Ruby 1.9.3
-  ruby::gem { "pry for 1.9.3":
+  ruby::gem { 'pry for 1.9.3':
     gem     => 'pry',
     ruby    => '1.9.3',
   }
-  ruby::gem { "pry-nav for 1.9.3":
+  ruby::gem { 'pry-nav for 1.9.3':
     gem     => 'pry-nav',
     ruby    => '1.9.3',
   }
-  ruby::gem { "pry-theme for 1.9.3":
+  ruby::gem { 'pry-theme for 1.9.3':
     gem     => 'pry-theme',
     ruby    => '1.9.3',
   }
-  ruby::gem { "awesome_print for 1.9.3":
+  ruby::gem { 'awesome_print for 1.9.3':
     gem     => 'awesome_print',
     ruby    => '1.9.3',
     version => '~> 1.2.0'
   }
 
   ## Ruby 2.0.0
-  ruby::gem { "pry for 2.0.0":
+  ruby::gem { 'pry for 2.0.0':
     gem     => 'pry',
     ruby    => '2.0.0'
   }
-  ruby::gem { "pry-nav for 2.0.0":
+  ruby::gem { 'pry-nav for 2.0.0':
     gem     => 'pry-nav',
     ruby    => '2.0.0',
   }
-  ruby::gem { "pry-theme for 2.0.0":
+  ruby::gem { 'pry-theme for 2.0.0':
     gem     => 'pry-theme',
     ruby    => '2.0.0',
   }
-  ruby::gem { "awesome_print for 2.0.0":
+  ruby::gem { 'awesome_print for 2.0.0':
     gem     => 'awesome_print',
     ruby    => '2.0.0',
   }
--- modules/projects/manifests/dabo_act.pp
+++ modules/projects/manifests/dabo_act.pp
@@ -16,15 +16,15 @@ class projects::dabo_act {
   }
 
   file { "${boxen::config::srcdir}/dabo_act/config/database.yml":
-      content => template("/opt/boxen/repo/modules/projects/templates/dabo_act/database.yml.erb"),
+      content => template('/opt/boxen/repo/modules/projects/templates/dabo_act/database.yml.erb'),
       require => Repository["${boxen::config::srcdir}/dabo_act"],
-      replace => "no"
+      replace => 'no'
   }
 
   file { "${boxen::config::srcdir}/dabo_act/.env":
-      content => template("/opt/boxen/repo/modules/projects/templates/dabo_act/env.erb"),
+      content => template('/opt/boxen/repo/modules/projects/templates/dabo_act/env.erb'),
       require => Repository["${boxen::config::srcdir}/dabo_act"],
-      replace => "no"
+      replace => 'no'
   }
 
   boxen::env_script { 'redistogo':
@@ -44,96 +44,96 @@ class projects::dabo_act {
   ## command => "${bundle} install'"
 
   ## bundle install
-  exec { "bundle install dabo_act":
-    provider => 'shell',
-    command => "${bundle} install'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
+  exec { 'bundle install dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} install'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
       Ruby::Gem["bundler for ${dabo_ruby_version}"],
       Service['postgresql']
     ],
-    unless => "${bundle} check'",
-    timeout => 1800
+    unless    => "${bundle} check'",
+    timeout   => 1800
   }
 
   ## rake db:setup
-  exec { "rake db:setup dabo_act":
-    provider => 'shell',
-    command => "${bundle} exec rake db:setup'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
-      Exec["bundle install dabo_act"]
+  exec { 'rake db:setup dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} exec rake db:setup'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
+      Exec['bundle install dabo_act']
     ]
   }
 
   ## rake db:sample_data
-  exec { "rake db:sample_data dabo_act":
-    provider => 'shell',
-    command => "${bundle} exec rake db:sample_data'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
-      Exec["rake db:setup dabo_act"],
+  exec { 'rake db:sample_data dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} exec rake db:sample_data'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
+      Exec['rake db:setup dabo_act'],
       Postgresql::Db['dabo_act_development']
     ]
   }
 
   ## rake db:fixtures:load
-  exec { "rake db:fixtures:load dabo_act":
-    provider => 'shell',
-    command => "${bundle} exec rake db:fixtures:load'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
-      Exec["rake db:sample_data dabo_act"],
+  exec { 'rake db:fixtures:load dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} exec rake db:fixtures:load'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
+      Exec['rake db:sample_data dabo_act'],
       Postgresql::Db['dabo_act_development']
     ]
   }
 
   ## rake parallel:create
-  exec { "rake parallel:create dabo_act":
-    provider => 'shell',
-    command => "${bundle} exec rake parallel:create'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
-      Exec["rake db:setup dabo_act"]
+  exec { 'rake parallel:create dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} exec rake parallel:create'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
+      Exec['rake db:setup dabo_act']
     ]
   }
 
   ## rake parallel:prepare
-  exec { "rake parallel:prepare dabo_act":
-    provider => 'shell',
-    command => "${bundle} exec rake parallel:prepare'",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
-      Exec["rake parallel:create dabo_act"]
+  exec { 'rake parallel:prepare dabo_act':
+    provider  => 'shell',
+    command   => "${bundle} exec rake parallel:prepare'",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
+      Exec['rake parallel:create dabo_act']
     ]
   }
 
   ## rake secret > .env
-  exec { "rake secret dabo_act":
-    provider => 'shell',
-    command => "echo DABO_RAILS_SECRET_KEY_BASE=`${bundle} exec rake secret'` >> .env",
-    cwd => "${boxen::config::srcdir}/dabo_act",
-    require => [
+  exec { 'rake secret dabo_act':
+    provider  => 'shell',
+    command   => "echo DABO_RAILS_SECRET_KEY_BASE=`${bundle} exec rake secret'` >> .env",
+    cwd       => "${boxen::config::srcdir}/dabo_act",
+    require   => [
       File["${boxen::config::srcdir}/dabo_act/.env"],
-      Exec["rake db:setup dabo_act"]
+      Exec['rake db:setup dabo_act']
     ],
-    unless => ["grep DABO_RAILS_SECRET_KEY_BASE .env"]
+    unless    => ['grep DABO_RAILS_SECRET_KEY_BASE .env']
   }
 
   ## ensure pg_stat_statements is loaded (needed for Heroku DB dumps)
-  exec { "psql CREATE EXTENSION pg_stat_statements":
-    provider => 'shell',
-    command  => "psql -p${postgresql::port} -c 'CREATE EXTENSION pg_stat_statements;'",
-    require  => [
+  exec { 'psql CREATE EXTENSION pg_stat_statements':
+    provider  => 'shell',
+    command   => "psql -p${postgresql::port} -c 'CREATE EXTENSION pg_stat_statements;'",
+    require   => [
       Postgresql::Db['dabo_act_development']
     ],
-    unless => ["psql -p${postgresql::port} -c '\\dx' | cut -d \\| -f1 | grep -w pg_stat_statements"]
+    unless    => ["psql -p${postgresql::port} -c '\\dx' | cut -d \\| -f1 | grep -w pg_stat_statements"]
   }
 
   # Mailcatcher gem needs to be installed outside of bundler
   ruby::gem { "mailcatcher for ${dabo_ruby_version}":
-    gem     => 'mailcatcher',
-    ruby    => $dabo_ruby_version,
+    gem       => 'mailcatcher',
+    ruby      => $dabo_ruby_version,
   }
 
 
