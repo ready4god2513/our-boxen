75968627ff437c0395648e6b228d8021e62d3a2f
Sean Knox <knoxville@gmail.com>
2014-09-11 18:51:08 -0700

    SK: Install Sublime Text 3 and setup

--- modules/people/manifests/seanknox/sublime.pp
+++ modules/people/manifests/seanknox/sublime.pp
@@ -1,21 +1,21 @@
 class people::seanknox::sublime {
 
-  include sublime_text_2
-  include sublime_text_2::config
+  include sublime_text
+  include sublime_text::config
 
-  $sublime_package_dir = "/Users/${::boxen_user}/Library/Application Support/Sublime Text 2/Packages"
+  $sublime_package_dir = "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages"
   $dotfiles = $people::seanknox::dotfiles
 
   file { [ "$sublime_package_dir/User" ]:
     ensure  => directory,
     require => [
-      Package['SublimeText2'],
+      Package['Sublime Text'],
     ]
   }
 
   file { "$sublime_package_dir/User/Preferences.sublime-settings":
     require   => [
-      Package['SublimeText2'],
+      Package['Sublime Text'],
       Repository["$dotfiles"],
       File["$sublime_package_dir/User"]
     ],
@@ -24,25 +24,15 @@ class people::seanknox::sublime {
 
   file { "$sublime_package_dir/User/Package Control.sublime-settings":
     require   => [
-      Package['SublimeText2'],
+      Package['Sublime Text'],
       Repository["$dotfiles"],
       File["$sublime_package_dir/User"]
     ],
     target => "$dotfiles/Package Control.sublime-settings"
   }
 
-  sublime_text_2::package { 'Package Control':
+  sublime_text::package { 'Package Control':
     source  => 'wbond/sublime_package_control',
-    require => Package['SublimeText2']
-  }
-
-  sublime_text_2::package { 'sublime-file-operations':
-    source  => 'chasetopher/sublime-file-operations',
-    require => Package['SublimeText2']
-  }
-
-  sublime_text_2::package { 'sublime-text-puppet':
-    source  => 'eklein/sublime-text-puppet',
-    require => Package['SublimeText2']
+    require => Package['Sublime Text']
   }
 }
