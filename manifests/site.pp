225ab584e66ca93a0cb2601a4953f8edbf835005
Fleming <daboeng@fleming.local>
2014-11-14 18:04:13 -0800

    Add Flowdock, iTerm, Alfred as default login apps

--- manifests/site.pp
+++ manifests/site.pp
@@ -232,4 +232,25 @@ node default {
       path    => '/Applications/Google Drive.app',
       hidden  => true;
   }
+
+  osx_login_item {
+    'Flowdock':
+      name    => 'Flowdock',
+      path    => '/Applications/Flowdock.app',
+      hidden  => true;
+  }
+
+  osx_login_item {
+    'iTerm':
+      name    => 'iTerm',
+      path    => '/Applications/iTerm.app',
+      hidden  => true;
+  }
+
+  osx_login_item {
+    'Alfred 2':
+      name    => 'Alfred 2',
+      path    => '/Applications/Alfred 2.app',
+      hidden  => true;
+  }
 }
