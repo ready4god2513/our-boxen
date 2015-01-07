19509123628c34c3acfb9169a419559cffddc6f8
Elli Balcomb & Jona Haga-Camacho <pair+ebalcomb+jonahaga@dabohealth.com>
2015-01-07 14:05:54 -0800

    Clear Chrome credentials #85575804
    Wake computer everyday at 3 a.m. on week days to
    run launchd to clear Chrome cookies and cache
    pairing stations.

--- modules/people/manifests/daboeng/pairing_station.pp
+++ modules/people/manifests/daboeng/pairing_station.pp
@@ -8,8 +8,29 @@ class people::daboeng::pairing_station {
   }
 
   file { "/Users/${::boxen_user}/git-completion.bash":
-    content => template('people/daboeng/pairing_shell/git-completion.bash.erb'),
-    replace => 'yes'
+    content   => template('people/daboeng/pairing_shell/git-completion.bash.erb'),
+    replace   => 'yes'
+  }
+
+  file { "${boxen::config::home}/bin/clear_chrome":
+    source    => "puppet:///${boxen::config::home}/repo/manifests/files/clear_chrome",
+    replace   => 'yes'
+  }
+
+  file { '/Library/LaunchDaemons/dev.clearchrome.plist':
+    source    => "puppet:///${boxen::config::home}/repo/manifests/files/dev.clearchrome.plist",
+    group     => 'wheel',
+    owner     => 'root',
+    notify    => Service['dev.clearchrome']
+  }
+
+  exec { "Wake computer at 3 a.m. to run clearchrome daemon":
+    command => "pmset repeat wakeorpoweron MTWRF 3:00:00",
+    user    => root
+  }
+
+  service { 'dev.clearchrome':
+    ensure    => running
   }
 
   osx_login_item {
