3fdfba982b5dab0fde998b5c9de18ab9778c0b12
Elli Balcomb & Jona Haga-Camacho <pair+ebalcomb+jonahaga@dabohealth.com>
2015-01-07 17:30:30 -0800

    Revert "Clear Chrome credentials #85575804"
    This reverts commit 24656f64f9804a0b59319b5a0a1b04e468399cb9.

--- modules/people/manifests/daboeng/pairing_station.pp
+++ modules/people/manifests/daboeng/pairing_station.pp
@@ -3,29 +3,13 @@ class people::daboeng::pairing_station {
   include flux::beta
 
   file { "/Users/${::boxen_user}/.profile":
-    content   => template('people/daboeng/pairing_shell/.profile.erb'),
-    replace   => 'yes'
+    content => template('people/daboeng/pairing_shell/.profile.erb'),
+    replace => 'yes'
   }
 
   file { "/Users/${::boxen_user}/git-completion.bash":
-    content   => template('people/daboeng/pairing_shell/git-completion.bash.erb'),
-    replace   => 'yes'
-  }
-
-  file { "${boxen::config::home}/bin/clear_chrome":
-    source    => "puppet:///${boxen::config::home}/repo/manifests/files/clear_chrome",
-    replace   => 'yes'
-  }
-
-  file { '/Library/LaunchDaemons/dev.clearchrome.plist':
-    source    => "puppet:///${boxen::config::home}/repo/manifests/files/dev.clearchrome.plist",
-    group     => 'wheel',
-    owner     => 'root',
-    notify    => Service['dev.clearchrome']
-  }
-
-  service { 'dev.clearchrome':
-    ensure    => running
+    content => template('people/daboeng/pairing_shell/git-completion.bash.erb'),
+    replace => 'yes'
   }
 
   osx_login_item {
